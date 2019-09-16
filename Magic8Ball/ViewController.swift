142d2ad217744981eb4e5e979a7d61a520cacd92
vamsiveeramasu <vamsiveeramasu@gmail.com>
2019-09-16 11:54:50 -0400

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Vamsi Veeramasu on 9/16/19.
+//  Copyright © 2019 Vamsi Veeramasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
